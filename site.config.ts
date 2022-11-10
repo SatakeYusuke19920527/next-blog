c16d1d21fe1aedf5369426f3ff3381d00b22aa3c
SatakeYusuke19920527 <satkyusk@icloud.com>
2022-11-10 09:31:21 +0900

    34. まで終了

---
+++ site.config.ts
@@ -0,0 +1,6 @@
+export const siteConfig = {
+  title: "Notion Next Blog",
+  twitterUrl: "https://twitter.com/fe_js_engineer",
+  InstagramUrl: "https://www.instagram.com/sat_yusuke/",
+  githubUrl: "https://github.com/SatakeYusuke19920527/next-blog",
+}
